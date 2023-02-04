fd25c8f2ac69aa0fa7358d21edbc05a8c0f3008b
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2023-02-04 16:35:09 +0100

    Update Balanced Array.py

--- Individual problems/Balanced Array.py
+++ Individual problems/Balanced Array.py
@@ -2,14 +2,18 @@ t = int(input())
      
 for i in range(t):
     n = int(input())
+
     if n/2 % 2 != 0:
         print('NO')
+     
     else:
-        even = [j for j in range(1, n+1) if j % 2 == 0]
+        even = [j for j in range(1, n + 1) if j % 2 == 0]
         odd = [j for j in range(1, n) if j % 2 != 0]
         odd[len(odd) - 1] = sum(even) - sum(odd[:-1])
+          
         if odd[len(odd) - 1] % 2 == 0:
             print('NO')
+               
         else:
             even.extend(odd)
             even = [str(i) for i in even]
