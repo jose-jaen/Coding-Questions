747abbefb1cf319890cd2939200432d1d6970f14
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2023-02-07 20:36:35 +0100

    Update Night at the Museum.py

--- Individual problems/Night at the Museum.py
+++ Individual problems/Night at the Museum.py
@@ -1,14 +1,14 @@
-n, m = [int(i) for i in input().split()]
-     
-i = 0
-if n < m:
-    print(n)
-elif n == m:
-    print(n+1)
-else:
-    while n >= 1:
-        n -= 1
-        i += 1
-        if i%m == 0 and i != 1:
-            n += 1
-    print(i)
+import string
+
+w = input()
+a = abs(string.ascii_lowercase.index(w[0]))
+b = 26 - string.ascii_lowercase.index(w[0])
+count = min(a, b)
+
+for i in range(1, len(w)):
+    a, b = string.ascii_lowercase.index(w[i - 1]), string.ascii_lowercase.index(w[i])
+    c = abs(a - b)
+    d = 26 - max(a, b) + min(a, b)
+    count += min(c, d)
+
+print(count)
