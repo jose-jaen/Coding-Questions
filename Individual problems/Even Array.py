87a1b9e935edc7841b622cafec8ba311f59e8a25
José Jaén <87200089+jose-jaen@users.noreply.github.com>
2023-02-08 09:53:48 +0100

    Update Even Array.py

--- Individual problems/Even Array.py
+++ Individual problems/Even Array.py
@@ -9,7 +9,7 @@ for i in range(t):
     res = [k for k in ids if k % 2 == 0]
     if len(x) % 2 == 0 and len(x) - len(ids) != len(x)/2:
         print(-1)
-    elif len(x) % 2 != 0 and len(x) - len(ids) != ceil(len(x)/2) or (len(x) == 1 and x[0]%2!=0):
+    elif len(x) % 2 != 0 and len(x) - len(ids) != ceil(len(x)/2) or (len(x) == 1 and x[0] %2 != 0):
         print(-1)
     else:
         print(len(res))
